a770c16677290a63d23213f7253dd665e91d8a67
Infinnitor <43831721+Infinnitor@users.noreply.github.com>
2021-07-26 23:00:08 +0100

    Create rectfinder functions (they actually work holy shiiiiiit)

--- LevelBuild/main.py
+++ LevelBuild/main.py
@@ -4,6 +4,7 @@ from gameinfo import game_info, pygame, sprite, random
 
 def text_clean(file):
     text = file.readlines()
+
     valid_text = []
     for f in range(len(text)):
         if text[f].startswith("#"):
@@ -42,6 +43,79 @@ class text_config():
             values[k] = char
 
         self.dict = values
+
+
+def rect_trailpath(game, start_tile):
+    x = start_tile.x
+    y = start_tile.y
+
+    trail = []
+    c_tile = start_tile
+
+    while c_tile.element == start_tile.element:
+        c_tile.c = (255, 40, 255)
+        trail.append(c_tile)
+        x += 1
+        try:
+            c_tile = game.sprites[y][x]
+
+        except IndexError:
+            break
+
+    return trail
+
+
+def rect_trail(game, given_trail):
+    target_element = given_trail[0].element
+
+    max_x = given_trail[-1].x
+
+    x = given_trail[0].x
+    y = given_trail[0].y + 1
+    if y >= level_height:
+        return None
+
+    trail = []
+    c_tile = game.sprites[y][x]
+
+    while c_tile.element == target_element and x <= max_x:
+        trail.append(c_tile)
+        x += 1
+        try:
+            c_tile = game.sprites[y][x]
+        except IndexError:
+            print(f"INDEXERROR Max: {max_x}, X: {x}, Y: {y}")
+
+        if len(trail) == len(given_trail):
+            print(f"LENGTH COMP Max: {max_x}, X: {x}, Y: {y}")
+            return trail
+
+    print(f"NONE Max: {max_x}, X: {x}, Y: {y}")
+    return None
+
+
+def rect_finder(given_e, game):
+    for y, row in enumerate(game.sprites):
+        for x, tile in enumerate(row):
+            if tile.element == given_e:
+                rect_trails = []
+                start_trail = rect_trailpath(game, tile)
+                rect_trails.extend(start_trail)
+
+                newtrail = rect_trail(game, start_trail)
+
+                while newtrail is not None:
+                    rect_trails.extend(newtrail)
+
+                    newtrail = rect_trail(game, newtrail)
+
+                for tile in rect_trails:
+                    tile.c = (0, 155, 0)
+                    tile.element = "Found"
+
+                return rect_trails
+
+    return None
 
 
 class grid_square(sprite):
@@ -88,12 +162,12 @@ config = text_config('colour_config.txt')
 level_image = pygame.image.load('level.png')
 level_image.set_colorkey((0, 0, 0))
 
-l_w, l_h = level_image.get_size()
+level_width, level_height = level_image.get_size()
 
 rows = []
-for y in range(l_h):
+for y in range(level_height):
     yrow = []
-    for x in range(l_w):
+    for x in range(level_width):
         px_colour = level_image.get_at((x, y))
 
         obj = grid_square(x, y, 20, px_colour)
@@ -104,8 +178,16 @@ for y in range(l_h):
 
 game.sprites = rows
 
+found_rects = []
+
 while game.run:
+
     game.update_keys()
+
+    if game.check_key(pygame.K_SPACE, buffer=True):
+        a_rect = rect_finder("GroundTerrain", game)
+        if a_rect is not None:
+            found_rects.append(a_rect)
 
     game.update_draw()
 
